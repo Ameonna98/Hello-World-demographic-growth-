1fadb7ae24b8801dc41298bf279cd11577feb1f3
Ameonna98 <44465868+Ameonna98@users.noreply.github.com>
2019-01-29 16:55:31 +0100

    Update and rename js/main.js to  js/main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $(function () {
 				let test = [];
 				// console.log(countrySel)	FUNZIONA
 				for (let i = 0; i < data.length; i++) {
-					if (countrySel == data[i].Country) {
+				 if (countrySel == data[i].Country) {
 						labels.push(countrySel);
 						test.push(data[i].a1960);
 						test.push(data[i].a1965);
@@ -42,17 +42,17 @@ $(function () {
 				var myChart = new Chart(ctx, {
 					type: 'bar',
 					"data": {
-						"labels": [1960,1965,1970,1975,1980,1985,1990,1995,2000,2005,2010,2015], // anni  dei dati del json 
+						"labels": [1960,1965,1970,1975,1980,1985,1990,1995,2000,2005,2010,2015], // anni dei dati del json 
 						"datasets": [
 							{
 								"label": " Crescita della popolazione demografica ",
 								"data": test,
 								// passo al grafico le percentuali scritte prima
-								"backgroundColor": ["#8EB4CF", "#7BA7C7", "#699BBF", "#568EB7",
-																		"#4382AF",	"#3176A8", "#2C6A97", "#275E86", 
-																		"#225376", "#1D4765", "#193B54", "#142F43",],
-								"borderColor": ["#8EB4CF", "#7BA7C7", "#699BBF", "#568EB7",
-																"#4382AF",	"#3176A8", "#2C6A97", "#275E86",
+								"backgroundColor": ["#8EB4CF", "#7BA7C7", "#699BBF", "#568EB7",    //scaletta 
+																		"#4382AF",	"#3176A8", "#2C6A97", "#275E86",   //dei 
+																		"#225376", "#1D4765", "#193B54", "#142F43",],  // colori
+								"borderColor": ["#8EB4CF", "#7BA7C7", "#699BBF", "#568EB7",        //cromotici
+																"#4382AF",	"#3176A8", "#2C6A97", "#275E86",       
 																 "#225376", "#1D4765", "#193B54", "#142F43",],
 								"borderWidth": 1
 							}
